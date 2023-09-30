c1730f79a25b248bdf03f1ade77cc73f6164f28e
PaulRaffoul <paul_raffoul24@hotmail.com>
2023-09-30 15:48:50 +0300

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass #decorator that creates init functions to classes
 class DataIngestionConfig:
     train_data_path : str=os.path.join('artifacts','train.csv') #path to store training data
@@ -55,4 +58,9 @@ if __name__=='__main__':
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+    
 
